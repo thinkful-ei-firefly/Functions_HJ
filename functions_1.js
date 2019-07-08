178d23f826b4e4581f83cdb9e7e17a05d058ae7c
Julianb12 <julianb1212@gmail.com>
2019-07-08 16:41:09 -0500

    drill 6

--- functions_1.js
+++ functions_1.js
@@ -1,12 +1,24 @@
 
 function getYearOfBirth (age) {
-    return 2019 - age;
+    return 2018 - age;
+    
+    
+    
 }
 
 function createGreeting(name, age) {
+    if (age < 0){
+        throw new Error('Age cannot be negative.')
+    }
+    
+    
     const yob = 2019 - age;
     return `I was born in ${yob}.`;
 }
 
-const greeting1 = createGreeting("Julian", 32);
+try { 
+const greeting1 = createGreeting("Julian", -32);
 console.log(greeting1);
+} catch(e) {
+    console.error(e.message)
+}
